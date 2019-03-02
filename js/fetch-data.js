080a0409d8ede272a33688c8e0d4995d9b94d5b2
Yong <yh849@cornell.edu>
2019-03-02 10:33:03 -0500

    update js

---
+++ js/fetch-data.js
@@ -0,0 +1,65 @@
+// Create a FHIR client (server URL, patient id in `demo`)
+var smart = FHIR.client(config);
+
+
+function fetchComorbidities(){
+  $("#table-data").empty();
+  var selected_conditions = [];
+
+  if(document.getElementById("Hypertensive disease").checked){
+    selected_conditions.push("64715009");
+  }
+  if(document.getElementById("diarrhea").checked){
+    selected_conditions.push("62315008");
+  }
+  if(document.getElementById("constipation").checked){
+    selected_conditions.push("14760008");
+  }
+
+  console.log(selected_conditions);
+
+  smart.api.search({
+    type: "Condition",
+    query: {code : {$or : selected_conditions}}
+  }).then(function(r){
+    var patients = [];
+    for (var i in r.data.entry){
+      patients.push(r.data.entry[i].resource.subject.reference.split("/")[1]);
+      // console.log(r.data.entry[i].resource.subject.reference.split("/")[1]);
+    }
+    console.log(patients);
+     // console.log(JSON.stringify(r.data.entry, null, 2));
+
+     smart.api.search({
+       type: "Condition",
+       query: {'subject' : {$or : patients}}
+     }).then(function(r){
+       var conditions = {};
+       for (var i in r.data.entry){
+         var condition_code = r.data.entry[i].resource.code.coding[0].code;
+         var condition_name = r.data.entry[i].resource.code.coding[0].display;
+         console.log(condition_code);
+         if (condition_code in conditions){
+           conditions[condition_code]["count"] += 1;
+         } else {
+           conditions[condition_code] = {
+             "count" : 1,
+             "name" : condition_name
+           };
+         }
+       }
+       console.log(JSON.stringify(conditions, null, 2));
+       for(var key in conditions){
+         $("#table-data").append(
+           '<tr>'+
+               '<td>'+conditions[key]["name"]+'</td>'+
+               '<td>'+key+'</td>'+
+               '<td>'+conditions[key]["count"]+'</td>'+
+           '</tr>'
+         )
+       }
+     });
+  });
+}
+
+// var conditions = fetchComorbidities(["14760008", "62315008", "21522001"])
